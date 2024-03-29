e4c77d3c4e4788d50256a9afc54cc716d4e97606
Sherif <mohamedgarieb551@yahoo.com>
2024-03-29 08:37:03 +0200

    Updated start script to run Java application

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -81,7 +81,7 @@ public class Main extends Application {
     Stage signupStage=  new Stage();
     String  setFiles = "";
     String pgRequest = "";
-    final String local = "http://localhost:8080";
+    final String local = "http://localhost:5080";
     final String heroku = "https://frozen-citadel-98016-4ac27ecd8250.herokuapp.com/";
     String urlFinal = heroku; //local //heroku
 //    Button signupEBtn = new Button();
